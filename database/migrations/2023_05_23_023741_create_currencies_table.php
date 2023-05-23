c955daf3e0aaf48b577391638a6a8b510ab2a50f
Mkharzom <76581486+Mkharzom@users.noreply.github.com>
2023-05-23 10:41:24 +0800

    models

---
+++ database/migrations/2023_05_23_023741_create_currencies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('logo')->nullable();
+            $table->string('sell')->nullable();
+            $table->string('last_sell')->nullable();
+            $table->string('buy')->nullable();
+            $table->string('last_buy')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
